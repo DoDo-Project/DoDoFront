f1ddd923574a8a490bc6a6a904c07cffa15f78aa
way <127731509+sooloin@users.noreply.github.com>
2026-02-27 22:19:22 +0900

    ⚙️ Chore: 초기 프로젝트 구조 및 개발 환경 구성 (alias, SVGR) (#8)

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import '@/app/styles/App.css';
+import DoDoLogo from '@/shared/assets/images/Logo_light.svg?react';
+
+function App() {
+  return (
+    <>
+      <DoDoLogo width={200} height={100} />
+    </>
+  );
+}
+
+export default App;
