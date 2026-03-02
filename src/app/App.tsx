3c659d9de98a9763507cd22c9872a1c3d2f877bd
sooloin <csb7543@naver.com>
2026-03-02 23:21:22 +0900

    :gear: chore: Tailwind v4 설정
    - 패키지 설치
    - Vite 플러그인 연결(vite.config.ts)
    - 전역 CSS에 Tailwind import 완료
    - 테스트 완료

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import DoDoLogo from '@/shared/assets/images/Logo_light.svg?react';
 function App() {
   return (
     <>
+      <p className="text-2xl font-bold text-blue-300">테일윈드 v4 테스트</p>
       <DoDoLogo width={200} height={100} />
     </>
   );
