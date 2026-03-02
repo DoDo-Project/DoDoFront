5dea3779f72b7886ed3cffdb806e1da5e127a95b
sooloin <csb7543@naver.com>
2026-03-02 23:58:15 +0900

    :gear: chore: 디자인 토큰(브랜드 컬러/폰트) 초기 설정
    - 브랜드 컬러 및 서브 컬러 설정
    - 폰트 설정(프리텐다드, Gmarket Sans)
    - App.css 내용 삭제
    - 테스트 완료
    
    Closes #12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,7 @@ import DoDoLogo from '@/shared/assets/images/Logo_light.svg?react';
 function App() {
   return (
     <>
-      <p className="text-2xl font-bold text-blue-300">테일윈드 v4 테스트</p>
-      <DoDoLogo width={200} height={100} />
+      <DoDoLogo width={200} height={100} className="mx-auto mt-10" />
     </>
   );
 }
