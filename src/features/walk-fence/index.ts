3f52a836bf7860f7399c3cdba49925e3ced77947
sooloin <csb7543@naver.com>
2026-06-15 17:49:33 +0900

    :sparkles: Feat: 실시간 위치 구독 훅(useLiveLocation) 구현
    - STOMP 연결 후 /sub/fence/location/{petId} 구독
    - 수신 payload를 location 상태로 관리, 연결 상태 노출
    - petId 변경/언마운트 시 연결 해제로 누수 방지
    
    `#62`

--- src/features/walk-fence/index.ts
+++ src/features/walk-fence/index.ts
@@ -9,6 +9,7 @@ export {
 export { useCreateFence } from './model/useCreateFence';
 export { useFenceBoundaries } from './model/useFenceBoundaries';
 export { useFenceStatus } from './model/useFenceStatus';
+export { useLiveLocation } from './model/useLiveLocation';
 export { useToggleFence } from './model/useToggleFence';
 export { useUpdateFenceRange } from './model/useUpdateFenceRange';
 export { FenceControlPanel } from './ui/FenceControlPanel';
